5aeb3d0e2d74f4aecc2166fa05c21ee89e188677
Wency <wencywego21@gmail.com>
2026-03-11 20:37:34 +0800

    modified models

--- app/models.py
+++ app/models.py
@@ -1,32 +1,33 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy import Integer, String, Boolean, ForeignKey, DateTime
 from app.database import Base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String, unique=True, index=True)
 
 class Seat(Base):
     __tablename__ = "seats"
 
-    id = Column(Integer, primary_key=True, index=True)
-    seat_number = Column(String, unique=True, index=True)
-    is_booked = Column(Boolean, default=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    seat_number: Mapped[str] = mapped_column(String, unique=True, index=True)
+    is_booked: Mapped[bool] = mapped_column(Boolean, default=False)
 
     # concurrency version control
-    version = Column(Integer, nullable=False, default=1)
+    version: Mapped[int] = mapped_column(Integer, nullable=False, default=1)
 
 class Booking(Base):
     __tablename__ = "bookings"
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    seat_id = Column(Integer, ForeignKey("seats.id"), unique=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    seat_id: Mapped[int] = mapped_column(Integer, ForeignKey("seats.id"), unique=True)
 
-    status = Column(String, default="PENDING")
-    expires_at = Column(DateTime)
+    status: Mapped[str] = mapped_column(String, default="PENDING")
+    expires_at: Mapped[datetime] = mapped_column(DateTime)
 
     user = relationship("User")
     seat = relationship("Seat")
