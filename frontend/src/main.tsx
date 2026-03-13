20a3cce8983ba3fe38b945ce619d3fc8470b3f4e
Wency <wencywego21@gmail.com>
2026-03-13 14:03:18 +0800

    added stripe payment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' // <--- Add this import
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 import App from './App'
 import './index.css'
 
+// public key for testing stripe fake payments
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* <--- Wrap your App here */}
+    <BrowserRouter>
+     <Elements stripe={stripePromise}>{/* <--- Wrap your App here */}
       <App />
+     </Elements>
     </BrowserRouter>
   </React.StrictMode>
 )
